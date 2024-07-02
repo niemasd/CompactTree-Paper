a4cf5c3fa0c12d8e38d6cac37f63bd5777eb2187
Niema Moshiri <niemamoshiri@gmail.com>
2024-07-01 18:28:30 -0700

    Fixed skbio

--- programs/benchmark_skbio.py
+++ programs/benchmark_skbio.py
@@ -2,10 +2,25 @@
 from skbio.tree import TreeNode
 from sys import argv
 from time import time
-total = 0.
+
+# load
+start = time()
 tree = TreeNode.read(argv[1])
+end = time()
+print(end - start)
+
+# preorder
+total = 0.
 start = time()
 for node in tree.preorder():
+    total += node.length
+end = time()
+print(end - start)
+
+# postorder
+total = 0.
+start = time()
+for node in tree.postorder():
     total += node.length
 end = time()
 print(end - start)
--- programs/load_skbio.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-from skbio.tree import TreeNode
-from sys import argv
-from time import time
-start = time()
-tree = TreeNode.read(argv[1])
-end = time()
-print(end - start)
