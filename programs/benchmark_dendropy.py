3155c099a21a520d606d49d1cfd170c52dd7b7a5
Niema Moshiri <niemamoshiri@gmail.com>
2024-07-01 18:24:54 -0700

    Fixed dendropy

--- programs/benchmark_dendropy.py
+++ programs/benchmark_dendropy.py
@@ -2,8 +2,23 @@
 from dendropy import Tree
 from sys import argv
 from time import time
-total = 0.
+
+# load
+start = time()
 tree = Tree.get(path=argv[1], schema='newick')
+end = time()
+print(end - start)
+
+# preorder
+total = 0.
+start = time()
+for node in tree.preorder_node_iter():
+    total += node.edge_length
+end = time()
+print(end - start)
+
+# postorder
+total = 0.
 start = time()
 for node in tree.preorder_node_iter():
     total += node.edge_length
--- programs/load_dendropy.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-from dendropy import Tree
-from sys import argv
-from time import time
-start = time()
-tree = Tree.get(path=argv[1], schema='newick')
-end = time()
-print(end - start)
