04667ec1e7301c56a5438d5d03d9398fb754102e
Niema Moshiri <niemamoshiri@gmail.com>
2024-07-01 18:27:24 -0700

    Fixed treeswift

--- programs/benchmark_treeswift.py
+++ programs/benchmark_treeswift.py
@@ -2,10 +2,25 @@
 from sys import argv
 from time import time
 from treeswift import read_tree_newick
-total = 0.
+
+# load
+start = time()
 tree = read_tree_newick(argv[1])
+end = time()
+print(end - start)
+
+# preorder
+total = 0.
 start = time()
 for node in tree.traverse_preorder():
+    total += node.edge_length
+end = time()
+print(end - start)
+
+# postorder
+total = 0.
+start = time()
+for node in tree.traverse_postorder():
     total += node.edge_length
 end = time()
 print(end - start)
