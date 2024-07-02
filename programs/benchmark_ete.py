271ea0a9b1886414264f8f36f14d9f8836d8861a
Niema Moshiri <niemamoshiri@gmail.com>
2024-07-01 18:26:13 -0700

    Added ETE

--- programs/benchmark_ete.py
+++ programs/benchmark_ete.py
@@ -2,10 +2,25 @@
 from ete4 import Tree
 from sys import argv
 from time import time
-total = 0.
+
+# load
+start = time()
 tree = Tree(open(argv[1]), parser=1)
+end = time()
+print(end - start)
+
+# preorder
+total = 0.
 start = time()
 for node in tree.traverse('preorder'):
+    total += node.dist
+end = time()
+print(end - start)
+
+# postorder
+total = 0.
+start = time()
+for node in tree.traverse('postorder'):
     total += node.dist
 end = time()
 print(end - start)
