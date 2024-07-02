84c88fc461f8e0520e7d57aed49f92d14773a644
Niema Moshiri <niemamoshiri@gmail.com>
2024-07-01 18:03:04 -0700

    Added pre and postorder

---
+++ programs/postorder_dendropy.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from dendropy import Tree
+from sys import argv
+from time import time
+total = 0.
+tree = Tree.get(path=argv[1], schema='newick')
+start = time()
+for node in tree.postorder_node_iter():
+    total += node.edge_length
+end = time()
+print(end - start)
---
+++ programs/preorder_skbio.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from skbio.tree import TreeNode
+from sys import argv
+from time import time
+total = 0.
+tree = TreeNode.read(argv[1])
+start = time()
+for node in tree.preorder():
+    total += node.length
+end = time()
+print(end - start)
